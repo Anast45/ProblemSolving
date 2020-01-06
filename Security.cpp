56134a50af7ce0cd9f92b89fd13bfd6b65e0daac
Anastasios D <48797123+Anast45@users.noreply.github.com>
2020-01-06 09:54:08 +0200

    Update Security.cpp

--- Security.cpp
+++ Security.cpp
@@ -8,25 +8,37 @@ void Alarm(bool k){
 
 int main() 
 {
+	//Declaration of variables
 	std::string A;
-    int G=0,T=0,M=0;
+    	int G=0,T=0,M=0;
+	
+	//input A(string)
 	std::cout << "input a string :";
 	std::cin >> A;
- 
+	
+	//from i=0 until i<=size of A
 	for (int i = 0; i <= A.size(); i++) {
 		if (A[i]=='G')
 		{
-            G=1;
-            if((M==1)&&(T==0)){Alarm(0);break;}
+            		G=1;
+			//if condition:means that $ has already been spotted and G is protecting the money
+			//exp:xxx$Gx...T
+            		if((M==1)&&(T==0)){Alarm(0);break;}
 		}
 		else if(A[i]=='T'){
-            T=1;G=0;
-            if(M==1){Alarm(1);break;}
+            		T=1;G=0;
+			//if condition:means that $ has already been spotted and G isnt protecting the money
+			//exp:xxG$Tx...G
+            		if(M==1){Alarm(1);break;}
 		}
 		else if(A[i]=='$'){
-		    M=1;
-		    if((T==1)&&(G==0)){Alarm(1);break;}
-		    else if((T==1)&&(G==1)){Alarm(0);break;}
+		    	M=1;
+			//if condition:means that $ has already been spotted and G isnt protecting the money
+			//exp:xxxT$x...G
+		    	if((T==1)&&(G==0)){Alarm(1);break;}
+			//else if condition:means that $ has already been spotted and G is protecting the money
+			//exp:xxxTG$xxx
+		    	else if((T==1)&&(G==1)){Alarm(0);break;}
 		}
 	}
 }
